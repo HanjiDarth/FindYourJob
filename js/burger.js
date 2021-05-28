b7ff67f9a4b76b5aba3bd6cf549a0844281d7e6e
Merenkov Daniil <dnlmrnkv@ya.ru>
2021-05-29 05:46:19 +0700

    new

--- js/burger.js
+++ js/burger.js
@@ -1,9 +1,26 @@
-const openMenu = document.querySelector('.open-button');
-const menu = document.querySelector('.navbar');
-const closeMenu = document.querySelector('.close-button');
+const menuElem = document.querySelector('.burger-menu');
+const humburgerElem = document.querySelector('.burger-button');
 
-const toggleMenu = (event) => {
-  menu.classList.toggle("navbar-open")
-};
- openMenu.addEventListener('click', toggleMenu);
- closeMenu.addEventListener('click', toggleMenu);
+const toggleMenu = () => {
+      menuElem.classList.toggle('burger-menu-active');
+      humburgerElem.classList.toggle('burger-button-active');
+
+      if (menuElem.classList.contains('burger-menu-active')) {
+        document.body.addEventListener('click', handlerMenu)
+      } else {
+        document.body.removeEventListener('click', handlerMenu)
+      }
+  };
+
+const handlerMenu = event => {
+  
+  const target = event.target;
+  const parent = target.closest('.burger-menu');
+
+  if ((!parent && target !== humburgerElem) ||
+    target.classList.contains('menu-link')) {
+      toggleMenu()
+    }
+}
+
+humburgerElem.addEventListener('click', toggleMenu);
