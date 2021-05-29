30972a3b6cbd275bb6226238f53e133ac2b86ad5
Merenkov Daniil <dnlmrnkv@ya.ru>
2021-05-30 06:52:14 +0700

    new

--- js/humburger.js
+++ js/humburger.js
@@ -1,13 +1,13 @@
-const menuElem = document.querySelector('.burger-menu');
-const humburgerElem = document.querySelector('.burger-button');
+const menuElem = document.querySelector('.humburger-menu');
+const humburgerElem = document.querySelector('.humburger-button');
 const firstElem = document.querySelector('.first-elem');
 
 const toggleMenu = () => {
-      menuElem.classList.toggle('burger-menu-active');
-      humburgerElem.classList.toggle('burger-button-active');
-      firstElem.classList.toggle('under-burger');
+      menuElem.classList.toggle('humburger-menu-active');
+      humburgerElem.classList.toggle('humburger-button-active');
+      firstElem.classList.toggle('under-humburger');
 
-      if (menuElem.classList.contains('burger-menu-active')) {
+      if (menuElem.classList.contains('humburger-menu-active')) {
         document.body.addEventListener('click', handlerMenu)
       } else {
         document.body.removeEventListener('click', handlerMenu)
@@ -17,7 +17,7 @@ const toggleMenu = () => {
 const handlerMenu = event => {
   
   const target = event.target;
-  const parent = target.closest('.burger-menu');
+  const parent = target.closest('.humburger-menu');
 
   if ((!parent && target !== humburgerElem) ||
     target.classList.contains('menu-link')) {
